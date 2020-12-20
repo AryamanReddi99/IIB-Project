af047f14b793376e3bd91d87fc68a327b2a719c6
Red <aryamanreddi@gmail.com>
2020-12-20 01:01:13 +0000

    Self-play metrics

--- Nim/Code/Models/QvQ/QvQ.py
+++ Nim/Code/Models/QvQ/QvQ.py
@@ -2,9 +2,11 @@ import os
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline, BSpline
 from nim_env_QvQ import *
 from evaluate_q_table import *
 
+os.chdir(os.path.dirname(__file__))
 i=3
 n=20
 env = nim_env_QvQ(i,n)
@@ -13,8 +15,8 @@ post_reward = 1
 q_table = np.zeros([(env.n)+4, env.action_space_n])
 
 # Hyperparameters
-alpha = 0.3
-gamma = 0.3
+alpha = 0.1
+gamma = 0.1
 epsilon = 1.0
 epsilon_min = 0.001
 epsilon_decay = 0.99
@@ -71,14 +73,33 @@ x = np.arange(window_size,trials + window_size, window_size)
 win_prob_1 = [window.count(1)/len(window) for window in winners]
 win_prob_minus_1 = [window.count(-1)/len(window) for window in winners]
 
+fig = plt.figure()
+xnew = np.linspace(x.min(), x.max(), 300)  
+spl_1 = make_interp_spline(x, win_prob_1, k=3)
+power_smooth_1 = spl_1(xnew)
+plt.plot(xnew, power_smooth_1, label="Player 1 win prob")
+spl_minus_1 = make_interp_spline(x, win_prob_minus_1, k=3)
+power_smooth_minus_1 = spl_minus_1(xnew)
+plt.plot(xnew, power_smooth_minus_1, label="Player 2 win prob")
 if optimal_table_reached:
     plt.axvline(x=optimal_iter, color='r', linestyle='-', linewidth = 2,label=f"optimal policy learned at iteration {optimal_iter}")
-plt.plot(x, win_prob_1,label="Player 1 win prob")
-plt.plot(x, win_prob_minus_1,label="Player 2 win prob")
 plt.xlabel("Trials")
 plt.ylabel(f"Win %")
-plt.title("Q-Learner self-play")
+plt.title(f"Q-Learner self-play, n = {n}")
 plt.legend()
-plt.show()
+fig.savefig(f"win_metrics_{n}.jpg")
+
+fig2 = plt.figure()
+if optimal_table_reached:
+    plt.axvline(x=optimal_iter, color='r', linestyle='-', linewidth = 2,label=f"optimal policy learned at iteration {optimal_iter}")
+xnew = np.linspace(0, trials, 300)  
+spl_table = make_interp_spline(np.arange(0,trials), table_abs_sum, k=3)
+power_smooth_table = spl_table(xnew)
+plt.plot(xnew, power_smooth_table)
+plt.xlabel("Trials")
+plt.ylabel("$\Sigma_(s, a) |Q(s, a)|$")
+plt.title(f"Q-Learner self-play, n = {n}")
+plt.legend()
+fig2.savefig(f"table_metrics_{n}.jpg")
 
 print("End")
